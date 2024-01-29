487680ffb24d92675e8013ff7ae1b06cb510b71a
joy <joy_ahmed_007@yahoo.com>
2024-01-30 04:25:03 +0600

    ✅Middleware Commented

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,45 +1,45 @@
-import { getToken } from 'next-auth/jwt';
-import { withAuth } from 'next-auth/middleware';
-import { NextResponse } from 'next/server';
+// import { getToken } from 'next-auth/jwt';
+// import { withAuth } from 'next-auth/middleware';
+// import { NextResponse } from 'next/server';
 
-export default withAuth(
-	async req => {
-		const pathname = req.nextUrl.pathname;
+// export default withAuth(
+// 	async req => {
+// 		const pathname = req.nextUrl.pathname;
 
-		// Manage route protection
-		const isAuthenticated = await getToken({ req });
+// 		// Manage route protection
+// 		const isAuthenticated = await getToken({ req });
 
-		const isLogingPage = pathname.startsWith('/signin');
+// 		const isLoginPage = pathname.startsWith('/signin');
 
-		const protectedRoutes = ['/dashboard'];
-		const isAccessingProtectedRoutes = protectedRoutes.some(route =>
-			pathname.startsWith(route)
-		);
+// 		const protectedRoutes = ['/dashboard'];
+// 		const isAccessingProtectedRoutes = protectedRoutes.some(route =>
+// 			pathname.startsWith(route)
+// 		);
 
-		if (isLogingPage) {
-			if (isAuthenticated) {
-				return NextResponse.redirect(new URL('/dashboard', req.url));
-			}
-			return NextResponse.next();
-		}
+// 		if (isLoginPage) {
+// 			if (isAuthenticated) {
+// 				return NextResponse.redirect(new URL('/dashboard', req.url));
+// 			}
+// 			return NextResponse.next();
+// 		}
 
-		if (!isAuthenticated && isAccessingProtectedRoutes) {
-			return NextResponse.redirect(new URL('/signin', req.url));
-		}
+// 		if (!isAuthenticated && isAccessingProtectedRoutes) {
+// 			return NextResponse.redirect(new URL('/signin', req.url));
+// 		}
 
-		if (pathname === '/') {
-			return NextResponse.redirect(new URL('/dashboard', req.url));
-		}
-	},
-	{
-		callbacks: {
-			async authorized() {
-				return true;
-			}
-		}
-	}
-);
+// 		if (pathname === '/') {
+// 			return NextResponse.redirect(new URL('/dashboard', req.url));
+// 		}
+// 	},
+// 	{
+// 		callbacks: {
+// 			async authorized() {
+// 				return true;
+// 			}
+// 		}
+// 	}
+// );
 
-export const config = {
-	matcher: ['/', '/signin', '/dashboard/:path*']
-};
+// export const config = {
+// 	matcher: ['/', '/signin', '/dashboard/:path*']
+// };
