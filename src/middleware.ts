aa076839b9ded037f8f113188cebe89ff6508909
joy <joy_ahmed_007@yahoo.com>
2024-01-31 04:02:31 +0600

    ✅Replaced middleware to its original state

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,47 +1,45 @@
-import { getToken } from 'next-auth/jwt';
-import { withAuth } from 'next-auth/middleware';
-import { NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt'
+import { withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
 
 export default withAuth(
   async function middleware(req) {
-    const pathname = req.nextUrl.pathname;
+    const pathname = req.nextUrl.pathname
 
     // Manage route protection
-    const isAuthenticated = await getToken({ req });
+    const isAuth = await getToken({ req })
+    const isLoginPage = pathname.startsWith('/login')
 
-    const isSignInPage = pathname.includes('/signin');
-    const isAccessingProtectedRoutes = pathname.includes('/dashboard');
+    const sensitiveRoutes = ['/dashboard']
+    const isAccessingSensitiveRoute = sensitiveRoutes.some((route) =>
+      pathname.startsWith(route)
+    )
 
-
-    if (isSignInPage) {
-      if (isAuthenticated) {
-        return NextResponse.redirect(new URL('/dashboard', req.url));
+    if (isLoginPage) {
+      if (isAuth) {
+        return NextResponse.redirect(new URL('/dashboard', req.url))
       }
-      return NextResponse.next();
+
+      return NextResponse.next()
     }
 
-    if (!isAuthenticated && isAccessingProtectedRoutes) {
-      return NextResponse.redirect(new URL('/signin', req.url));
+    if (!isAuth && isAccessingSensitiveRoute) {
+      return NextResponse.redirect(new URL('/login', req.url))
     }
 
-    if (pathname === '/' ) {
-      if (isAuthenticated) {
-        return NextResponse.redirect(new URL('/dashboard', req.url));
-      } else {
-        return NextResponse.redirect(new URL('/signin', req.url));
-      }
+    if (pathname === '/') {
+      return NextResponse.redirect(new URL('/dashboard', req.url))
     }
   },
   {
     callbacks: {
       async authorized() {
-        return true;
+        return true
       },
     },
   }
-);
-
+)
 
 export const config = {
-	matcher: ['/', '/signin', '/dashboard/:path*']
-};
+  matchter: ['/', '/login', '/dashboard/:path*'],
+}
