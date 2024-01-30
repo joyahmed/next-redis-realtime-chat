19de341056e15b9238a4a07ca35e80da25de9b3e
joy <joy_ahmed_007@yahoo.com>
2024-01-31 02:43:24 +0600

    ❌Date-Fns

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,48 +3,43 @@ import { withAuth } from 'next-auth/middleware';
 import { NextResponse } from 'next/server';
 
 export default withAuth(
-	async req => {
-		const pathname = req.nextUrl.pathname;
+  async (req) => {
+    const pathname = req.nextUrl.pathname;
 
-		// Manage route protection
-		const isAuthenticated = await getToken({ req });
+    // Manage route protection
+    const isAuthenticated = await getToken({ req });
 
-		const isLoginPage = pathname.startsWith('/signin');
-
-		const protectedRoutes = ['/dashboard'];
-		const isAccessingProtectedRoutes = protectedRoutes.some(route =>
-			pathname.startsWith(route)
-    );
+    const isSignInPage = pathname.includes('/signin');
+    const isAccessingProtectedRoutes = pathname.includes('/dashboard');
 
 
-
-		if (isLoginPage) {
-			if (isAuthenticated) {
-				return NextResponse.redirect(new URL('/dashboard', req.url));
-			}
-			return NextResponse.next();
-		}
+    if (isSignInPage) {
+      if (isAuthenticated) {
+        return NextResponse.redirect(new URL('/dashboard', req.url));
+      }
+      return NextResponse.next();
+    }
 
     if (!isAuthenticated && isAccessingProtectedRoutes) {
+      return NextResponse.redirect(new URL('/signin', req.url));
+    }
 
-      console.log(`isAccessingProtectedRoutes => =>`, isAccessingProtectedRoutes);
+    if (pathname === '/') {
+      return NextResponse.redirect(new URL('/dashboard', req.url));
+    }
 
-      console.log(`pathname => =>`, pathname);
-			return NextResponse.redirect(new URL('/signin', req.url));
-		}
 
-		if (pathname === '/') {
-			return NextResponse.redirect(new URL('/dashboard', req.url));
-		}
-	},
-	{
-		callbacks: {
-			async authorized() {
-				return true;
-			}
-		}
-	}
+
+  },
+  {
+    callbacks: {
+      async authorized() {
+        return true;
+      },
+    },
+  }
 );
+
 
 export const config = {
 	matcher: ['/', '/signin', '/dashboard/:path*']
